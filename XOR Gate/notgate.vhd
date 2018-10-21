3e360be9d86072f444c8fa926d97fc90c10593c8
Kalun Lau <klnlau@gmail.com>
2018-10-20 21:51:05 -0500

    2-input XOR Gate

---
+++ XOR Gate/notgate.vhd
@@ -0,0 +1,15 @@
+-- Library and Use statements for IEEE packages
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity LaNegadora is
+	port(	inape, inbpe	:	in std_logic;
+			outpe	:	out std_logic);
+end LaNegadora;
+
+architecture funca of LaNegadora is
+begin
+	outpe <= not (inape xor inbpe) ;
+end funca;
+
